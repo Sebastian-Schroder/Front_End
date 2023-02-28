9a75aea6857819634937c801e8c0e67b2f075f74
DARKMOONlite <sebastianschroder@hotmail.com>
2023-02-28 23:05:54 +1100

    ++

---
+++ Hacker_Effect/basic.js
@@ -0,0 +1,31 @@
+
+
+const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+// a = document.getElementsByTagName("h1")
+// for(i in a){
+//     a[i].onmouseover = event
+// }
+
+document.querySelector("h1").onmouseover = event =>{
+    let iterations =0;
+
+    const interval = setInterval(()=>{
+        event.target.innerText = event.target.innerText.split("")
+        .map((letter,index)=>{
+            
+            if(index<iterations){
+                return event.target.dataset.value[index];
+            }
+            
+            return letters[Math.floor(Math.random()*26)]
+            
+        })
+        .join("");
+        iterations+= 1/3;
+        
+
+        if(iterations>event.target.dataset.value.length) {clearInterval(interval)};
+
+    },30);
+
+}; 
